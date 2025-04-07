cc4730b67fa6aef5f8738a38519bde0c6d58dc8c
ram16 <thomass.montaine@gmail.com>
2025-04-07 02:29:04 +0200

    interface abstraite de terminal

---
+++ Terminal/Terminal.cs
@@ -0,0 +1,29 @@
+﻿namespace _COBRA_
+{
+    public interface ITerminal
+    {
+        public static ITerminal instance;
+
+        //----------------------------------------------------------------------------------------------------------
+
+        protected virtual void Awake()
+        {
+            instance = this;
+        }
+
+        //----------------------------------------------------------------------------------------------------------
+        
+        public void Toggle(in bool toggle)
+        {
+
+        }
+
+        //----------------------------------------------------------------------------------------------------------
+
+        protected virtual void OnDestroy()
+        {
+            if (this == instance)
+                instance = null;
+        }
+    }
+}
