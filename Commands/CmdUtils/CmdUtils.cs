53008e12ffa45e4a32d38ded642104f8f61a8d46
ram16 <thomass.montaine@gmail.com>
2025-04-08 16:34:00 +0200

    added 'skip' command

--- Commands/CmdUtils/CmdUtils.cs
+++ Commands/CmdUtils/CmdUtils.cs
@@ -1,4 +1,5 @@
 ﻿using _ARK_;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace _COBRA_
@@ -30,6 +31,41 @@ namespace _COBRA_
                 manual: new("clear all previous entries"),
                 action: exe => NUCLEOR.delegates.onStartOfFrame_once += Command.Line.ClearHistory
                 ));
+
+            Command.cmd_root_shell.AddCommand(new(
+                "skip",
+                manual: new("skip <int> first entries from pipe"),
+                pipe_min_args_required: 1,
+                args: exe =>
+                {
+                    if (exe.root.line.TryReadArgument(out string arg))
+                        if (int.TryParse(arg, out int count))
+                            exe.args.Add(count);
+                        else
+                            exe.root.error = $"could not parse into int value: '{arg}'";
+                    exe.args.Add(0);
+                },
+                on_pipe: (exe, data) =>
+                {
+                    int skips = (int)exe.args[0];
+                    int iterations = (int)exe.args[1];
+
+                    switch (data)
+                    {
+                        case IEnumerable<string> strings:
+                            foreach (string str in strings)
+                                if (iterations++ >= skips)
+                                    exe.Stdout(str);
+                            break;
+
+                        default:
+                            if (iterations++ >= skips)
+                                exe.Stdout(data);
+                            break;
+                    }
+
+                    exe.args[1] = iterations;
+                }));
         }
     }
 }
