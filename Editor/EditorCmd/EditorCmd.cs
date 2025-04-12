2e1c0679e6a0636c6db4c913c77e114f4c12eb0a
ram16 <thomass.montaine@gmail.com>
2025-04-12 22:37:50 +0200

    commit auto: réparations

--- Editor/EditorCmd/EditorCmd.cs
+++ Editor/EditorCmd/EditorCmd.cs
@@ -1,5 +1,6 @@
 using _COBRA_;
 using _UTIL_e;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace _COBRA_e
@@ -9,19 +10,38 @@ namespace _COBRA_e
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
         static void OnAfterSceneLoad()
         {
-            Shell.static_domain.AddAction(
+            const string
+                flag_blocking = "--blocking";
+
+            Shell.static_domain.AddRoutine(
                 "push-all-git-repos",
                 min_args: 1,
+                opts: static exe =>
+                {
+                    if (exe.line.TryRead_one_flag(exe, out string flag, flag_blocking))
+                        exe.opts.Add(flag_blocking, null);
+                },
                 args: static exe =>
                 {
                     if (exe.line.TryReadArgument(out string arg))
                         exe.args.Add(arg);
                 },
-                action: static exe =>
-                {
-                    string arg = (string)exe.args[0];
+                routine: ERoutine);
+
+            static IEnumerator<CMD_STATUS> ERoutine(Command.Executor exe)
+            {
+                string arg = (string)exe.args[0];
+                bool blocking = exe.opts.ContainsKey(flag_blocking);
+
+                if (blocking)
                     GitBatchPusher.PushAllGitRepos(arg);
-                });
+                else
+                {
+                    var routine = GitBatchPusher.EPushAllGitRepos(arg);
+                    while (routine.MoveNext())
+                        yield return new CMD_STATUS(CMD_STATES.BLOCKING, progress: routine.Current);
+                }
+            }
         }
     }
 }
