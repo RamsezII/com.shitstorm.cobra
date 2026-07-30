d51a55d87faef85b6d5bf9f9a46a80983a9d8c2b
ram16 <thomass.montaine@gmail.com>
2026-07-30 21:09:29 +0200

    ArkPaths methodes

--- Runtime/Core/Shell/_Workdir.cs
+++ Runtime/Core/Shell/_Workdir.cs
@@ -5,7 +5,7 @@ namespace _COBRA_
 {
     partial class Shell
     {
-        public readonly ValueHandler<string> workdir = new(ArkPaths.instance.Value.dpath_home);
+        public readonly ValueHandler<string> workdir = new(ArkMachine.DFHome.FullName);
 
         public ExecutionStatus RegularStatus() => new(
             code: CMD_STATUS.WAIT_FOR_STDIN,
